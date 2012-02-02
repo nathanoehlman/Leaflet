2489d8101e1885830fb341690f22b6e9f6845d8e
Nathan Oehlman <nathan.oehlman@sidelab.com>
2012-02-02 14:54:45 +1000

    Fixed compilation of EPSG3395 transform

--- src/geo/crs/CRS.EPSG3395.js
+++ src/geo/crs/CRS.EPSG3395.js
@@ -1,12 +1,14 @@
+function getEPSG3395Transform() {
+    var m = L.Projection.Mercator,
+		r = m.R_MAJOR,
+		r2 = m.R_MINOR;
+	
+	return new L.Transformation(0.5 / (Math.PI * r), 0.5, -0.5 / (Math.PI * r), 0.5);
+}
+
 L.CRS.EPSG3395 = L.Util.extend({}, L.CRS, {
 	code: 'EPSG:3395',
 
 	projection: L.Projection.Mercator,
-	transformation: (function () {
-		var m = L.Projection.Mercator,
-			r = m.R_MAJOR,
-			r2 = m.R_MINOR;
-		
-		return new L.Transformation(0.5/(Math.PI * r), 0.5, -0.5/(Math.PI * r), 0.5);
-	})()
+	transformation: getEPSG3395Transform()
 });
